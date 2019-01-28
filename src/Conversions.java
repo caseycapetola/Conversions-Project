79c0415cbc05059c530d5e7bbb6c08ea1bdf09d8
Casey <Casey@PILLAR.fios-router.home>
2019-01-28 13:40:17 -0500

    Tested methods

--- src/Conversions.java
+++ src/Conversions.java
@@ -4,7 +4,10 @@ public class Conversions
 
 	public static void main(String[] args) 
 	{
-		
+		System.out.println("8 ft = " + feetToInches(8) + " in");
+		System.out.println("3 mi = " + milesToFeet(3) + " ft");
+		System.out.println("5 yds = " + yardsToMeters(5) + " m");
+		System.out.println("70 lbs = " + poundsToKilograms(70) + " kg");
 
 	}
 
